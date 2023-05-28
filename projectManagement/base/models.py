7d271ff8811c21e469d303de3c6a1fb62dda8209
HamzabinSaqib <hbsdesigning@gmail.com>
2023-05-28 18:02:07 +0500

    Add Project Model & Register on Admin

--- projectManagement/base/models.py
+++ projectManagement/base/models.py
@@ -1,3 +1,36 @@
+
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.utils import timezone
+from datetime import datetime
+import uuid
 
 # Create your models here.
+
+class Project(models.Model):
+    
+    projID = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    projName = models.CharField(max_length=50)
+    projDesc = models.TextField(null=True, blank=True)
+    startDate = models.DateTimeField(auto_now_add=True)
+    dueDate = models.DateTimeField(null=True, blank=True)
+    endDate = models.DateTimeField(null=True, blank=True)
+    projStatus = models.CharField(max_length=20)
+    
+    def set_projStatus(self, stat: str): # In Progress - On Hold - Completed - Cancelled - Overdue - Inactive
+        """Set Project Status"""
+        self.projStatus = stat
+        self.save()
+    
+    def set_dueDate(self, date: str): # 'YYYY-DD-MM HH:MM:SS'
+        """Set Project Due Date"""
+        self.dueDate = datetime.strptime(date, '%Y-%d-%m %H:%M:%S')
+        
+    def __str__(self):
+        return self.projName
+    
+    def save(self, *args, **kwargs):
+        if (self.projStatus == 'Completed' or self.projStatus == 'Cancelled') and not self.endDate:
+            self.endDate = timezone.now()
+        return super().save(*args, **kwargs)
+    

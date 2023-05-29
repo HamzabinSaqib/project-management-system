393d1e1e38f47e1f19ce3f1a71c14fc5bd346e02
Romaiz Dabeer <romaizdabeer57@gmail.com>
2023-05-30 00:12:18 +0500

    Update_1

---
+++ projectManagement/authentication/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def home(request):
+    return render(request, 'home.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('authentication:home')
+        else:
+            messages.error(request, 'Invalid Username or Password')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully. You can now log in.')
+            return redirect('authentication:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'sign_up.html', {'form': form})

29834dd591b7027e3ab0e7be94d3710ee95794b7
HamzabinSaqib <hbsdesigning@gmail.com>
2023-05-31 02:20:22 +0500

    Login Required for Direct Page Access

--- projectManagement/base/views.py
+++ projectManagement/base/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from . models import Project
 
-def login(req):
-    return render(req, 'base/login.html')
-
+@login_required(login_url='authentication:login')
 def home(req):
     projects = Project.objects.all()
     context = {'projects':projects}
     return render(req, 'base/home.html', context)
 
+@login_required(login_url='authentication:login')
 def project(req, pk):
     project = Project.objects.get(projID=pk)
     context = {'project': project}
     return render(req, 'base/project.html', context)
+

176d4070057b8d1c7c26ab661d86a64e81c658b3
Hamza bin Saqib <hbsdesigning@gmail.com>
2024-01-16 22:04:53 +0500

    Add Test Cases

--- projectManagement/authentication/tests.py
+++ projectManagement/authentication/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.home_url = reverse('authentication:home')
+        self.login_url = reverse('authentication:login')
+        self.logout_url = reverse('authentication:logout')
+        self.signup_url = reverse('authentication:signup')
+        self.check_username_url = reverse('authentication:check_username')
+
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+
+    def test_home_GET(self):
+        response = self.client.get(self.home_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'authentication/home.html')
+
+    def test_login_GET(self):
+        self.client.logout()
+        response = self.client.get(self.login_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'authentication/login.html')
+
+    def test_logout_GET(self):
+        response = self.client.get(self.logout_url)
+
+        self.assertEquals(response.status_code, 302)  # Redirects to home
+
+    def test_signup_GET(self):
+        response = self.client.get(self.signup_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'authentication/sign_up.html')
+
+    def test_check_username_POST(self):
+        response = self.client.post(self.check_username_url, {'username': 'newuser'}, content_type='application/json')
+
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.json(), {'available': True})
--- projectManagement/base/tests.py
+++ projectManagement/base/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Project
 
-# Create your tests here.
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.home_url = reverse('base:home')
+        self.new_project_url = reverse('base:new_Project')
+
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+
+        self.project = Project.objects.create(
+            manager=self.user,
+            projName='Test Project',
+            projDesc='Test Project Description',
+            projStatus='In Progress',
+        )
+        
+        self.project_url = reverse('base:project', args=[str(self.project.projID)])
+        self.delete_project_url = reverse('base:del_project', args=[str(self.project.projID)])
+
+    def test_home_GET(self):
+        response = self.client.get(self.home_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base/home.html')
+
+    def test_project_GET(self):
+        response = self.client.get(self.project_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base/project.html')
+
+    def test_new_project_GET(self):
+        response = self.client.get(self.new_project_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base/project_creation.html')
+
+    def test_delete_project_POST(self):
+        response = self.client.post(self.delete_project_url)
+
+        self.assertEquals(response.status_code, 302)  # Redirects to home
